64de1aecf4aea01848d2c7fcb004ed4caa351079
Nhat-Original <nhatoriginal@gmail.com>
2024-03-10 22:18:57 +0700

    fix: modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     remotePatterns: [{ hostname: 'lh3.googleusercontent.com' }],
   },
