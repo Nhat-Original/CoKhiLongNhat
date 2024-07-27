1af3305d8ee91d999d19beec326f1b54bb6bbfd9
Nhat-Original <nhatoriginal@gmail.com>
2024-07-27 21:59:03 +0700

    chore: remove refetch on window focus, added header in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
             key: 'Access-Control-Allow-Origin',
             value: process.env.ALLOWED_ORIGIN || '*',
           },
