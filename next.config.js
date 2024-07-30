686f340881e09619f89b9f3154316b7fa1410a80
Nhat-Original <nhatoriginal@gmail.com>
2024-07-30 14:09:17 +0700

    chore: small changes in cors

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: process.env.ALLOWED_ORIGIN || '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
